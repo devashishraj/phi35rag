1f8eca300a737933e4686ae416a25ff22072b603
devashishRaj <29112354+devashishRaj@users.noreply.github.com>
2024-12-14 20:48:07 +0530

    misc fix

--- fetchData/fetch.sh
+++ fetchData/fetch.sh
@@ -6,9 +6,9 @@ python getWikiChangesToJson.py
 #move output to sharedDir
 echo "moving output to ${OUTPUT_DIR}/step to generate quereis for RAG "
 
-mv wikipedia_article_changes.json ${INPUT_DIR}/model_repo/Step1/generateQueries/
+mv wikipedia_article_changes.json ${INPUT_DIR}/input1/phi35ragRepo/generateQueries/
 
 #moving only relevant part to next stage
-mv ${INPUT_DIR}/model_repo/step1/generateQueries/ ${OUTPUT_DIR}/step2/
-mv ${INPUT_DIR}/model_repo/step1/processData/ ${OUTPUT_DIR}/step2/
-mv ${INPUT_DIR}/ragRepo/step1/ragPrompt/ ${OUTPUT_DIR}/step2/
+mv ${INPUT_DIR}}/input1/phi35ragRepo/generateQueries/ ${OUTPUT_DIR}/ouput1/
+mv ${INPUT_DIR}}/input1/phi35ragRepo/processData/ ${OUTPUT_DIR}/output1/
+mv ${INPUT_DIR}}/input1/phi35ragRepo/ragPrompt/ ${OUTPUT_DIR}/ouput1/
