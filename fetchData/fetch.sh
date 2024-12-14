e3abae9a93d7c91ebc329fe918a888e7fe3399fb
devashishRaj <29112354+devashishRaj@users.noreply.github.com>
2024-12-14 22:10:32 +0530

    msic fix

--- fetchData/fetch.sh
+++ fetchData/fetch.sh
@@ -1,14 +1,14 @@
 # Run the data fetch script
 
-echo "getWikiChangesToJson.py"
+echo "running getWikiChangesToJson.py script to fetch data"
 python getWikiChangesToJson.py
 
 #move output to sharedDir
-echo "moving output to ${OUTPUT_DIR}/step to generate quereis for RAG "
+echo "moving output to ${OUTPUT_DIR}/ouput1 to generate quereis for RAG "
 
 mv wikipedia_article_changes.json ${INPUT_DIR}/input1/phi35ragRepo/queryGen/
 
 #moving only relevant part to next stage
-mv ${INPUT_DIR}}/input1/phi35ragRepo/queryGen/ ${OUTPUT_DIR}/ouput1/
-mv ${INPUT_DIR}}/input1/phi35ragRepo/dataEmbed/ ${OUTPUT_DIR}/output1/
-mv ${INPUT_DIR}}/input1/phi35ragRepo/rag/ ${OUTPUT_DIR}/ouput1/
+mv ${INPUT_DIR}}/input1/phi35ragRepo/queryGen ${OUTPUT_DIR}/ouput1/
+mv ${INPUT_DIR}}/input1/phi35ragRepo/dataEmbed ${OUTPUT_DIR}/output1/
+mv ${INPUT_DIR}}/input1/phi35ragRepo/rag ${OUTPUT_DIR}/ouput1/
