65ce6eb5a926772763e19f11b510fa68b6b5a4a3
devashishRaj <29112354+devashishRaj@users.noreply.github.com>
2025-02-23 23:13:02 +0530

    add embedding model type torch.bfloat16

--- saveModels.py
+++ saveModels.py
@@ -1,6 +1,14 @@
 from sentence_transformers import SentenceTransformer
-
-model = SentenceTransformer("jinaai/jina-embeddings-v3",trust_remote_code=True)
-
+import torch
 saveModelTo="/app/jinv3"
+
+modelKwargs={"torch_dtype":torch.bfloat16}
+model = SentenceTransformer(
+        "jinaai/jina-embeddings-v3",
+        trust_remote_code=True,
+        model_kwargs=modelKwargs,
+    )
+
 model.save(saveModelTo,safe_serialization=True)
+
+
